d74100a8f9072f5c2f7a8036af8df8882dd34256
Lyall <58636255+Lyall-A@users.noreply.github.com>
2024-07-25 10:12:31 +0100

    nvidia

--- startup.sh
+++ startup.sh
@@ -3,7 +3,7 @@
 sleep 5
 
 # Apply NVIDIA settings
-/usr/bin/nvidia-settings -l
+if [ -f /usr/bin/nvidia-settings ]; then /usr/bin/nvidia-settings -l; fi
 
 # Setup audio
 ./Arch-Linux/Audio\ Stuff/setup-audio.sh
