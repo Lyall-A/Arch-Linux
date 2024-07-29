91889ea9d78cd809db43a3d7305bba5126534a49
Lyall <lyall.yeet@gmail.com>
2024-07-29 22:17:17 +0100

    forgot to remove print

--- Audio Stuff/monitor-audio.sh
+++ Audio Stuff/monitor-audio.sh
@@ -20,7 +20,6 @@ while true; do
         linked_to_other=$(echo "$dump" | jq -r --arg id "$id" --arg sink_id "$sink_id" 'any(.[]; .type == "PipeWire:Interface:Link" and .info["output-node-id"] == ($id | tonumber) and .info["input-node-id"] != ($sink_id | tonumber))')
         # echo "Node ID: ${id}, linked to default sink: ${linked_to_sink}, linked to other: ${linked_to_other}"
 
-        echo "$linked_to_sink $linked_to_other"
         # Unlink from default sink if linked to anything else
         if [[ "$linked_to_sink" = true && "$linked_to_other" = true ]]; then
             echo "Unlinking $id from default sink"
